282610272c63f22a1dc49514e5f5157eef24d443
Heather <hnrogers10@gmail.com>
2022-10-31 16:36:00 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackTheRobotPt1
+{
+    class Primary
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Simple test");
+
+            Working robot = new Working();
+            
+        }
+    }
+}
---
+++ Working.cs
@@ -0,0 +1,10 @@
+class Working
+{
+    private float x = 0;
+    private float y = 0;
+
+    public void RobotTrack(string[] directions)
+    {
+        
+    }
+}
